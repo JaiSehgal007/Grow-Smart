1930b57119d0f631b286e75739d362eccd581ed5
JaiSehgal007 <jaisehgal511@gmail.com>
2025-02-10 03:00:50 +0530

    added api documentation using OpenAPI

--- api-gateway/src/main/java/com/amangoes/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/amangoes/gateway/config/SecurityConfig.java
@@ -2,16 +2,19 @@ package com.amangoes.gateway.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configurable
+@Configuration
 public class SecurityConfig {
+
+    private final String[] freeResourceUrls = {"/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**", "/api-docs/**", "/aggregate/**"};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
-        return http.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+        return http.authorizeHttpRequests(authorize -> authorize.requestMatchers(freeResourceUrls).permitAll().anyRequest().authenticated())
             .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
     }
 
