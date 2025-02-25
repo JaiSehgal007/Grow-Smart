1d42303c5731c39d256661a2b725d1a5ebb16095
JaiSehgal007 <jaisehgal511@gmail.com>
2025-02-25 16:07:04 +0530

    created notification service and integrated kafka

---
+++ notification-service/src/main/java/com/amangoes/notification/service/NotificationService.java
@@ -0,0 +1,45 @@
+package com.amangoes.notification.service;
+
+import com.amangoes.order.event.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    private final JavaMailSender javaMailSender;
+
+    @KafkaListener(topics = "order-placed")
+    public void Listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Got message from order-placed topic {}", orderPlacedEvent);
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("jai@amangoes.com");
+            messageHelper.setTo(orderPlacedEvent.getEmail().toString());
+            messageHelper.setSubject("Your Order Number %s has been placed".formatted(orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                    Hi
+                    
+                    Your order with order number %s has been placed successfully.
+                    
+                    Best Regards
+                    Amangoes Team
+                    """,
+                    orderPlacedEvent.getOrderNumber()));
+        };
+
+        try {
+            javaMailSender.send(messagePreparator);
+        } catch (MailException e) {
+            log.error("Error while sending email", e);
+        }
+    }
+}
--- order-service/src/main/java/com/amangoes/order/service/OrderService.java
+++ order-service/src/main/java/com/amangoes/order/service/OrderService.java
@@ -2,21 +2,27 @@ package com.amangoes.order.service;
 
 import com.amangoes.order.client.InventoryClient;
 import com.amangoes.order.dto.OrderRequest;
+import com.amangoes.order.event.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import com.amangoes.order.model.Order;
 import com.amangoes.order.repository.OrderRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
     private final OrderRepository orderRepository;
 
     private final InventoryClient inventoryClient;
+
+    private final KafkaTemplate<String,OrderPlacedEvent> kafkaTemplate; // it uses key (name of the topic to send the event to),  value (is the event to be sent)
 
     public void placeOrder(OrderRequest orderRequest){
 
@@ -31,6 +37,13 @@ public class OrderService {
 
             // save Order object to OrderRepository
             orderRepository.save(order);
+
+            //send message to Kafka Topic
+            // requires orderNumber and email
+            OrderPlacedEvent orderPlacedEvent=new OrderPlacedEvent(order.getOrderNumber(),orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed",orderPlacedEvent);
+            kafkaTemplate.send("order-placed",orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed",orderPlacedEvent);
         }else{
             throw new RuntimeException("Product with skuCode :"+ orderRequest.skuCode() +" out of stock");
         }
